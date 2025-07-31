14222fa99b27ece9ba1e2be309b258067ecc50da
Octar123 <ranjanassmali123@gmail.com>
2025-07-31 18:52:33 +0530

    training data downloading

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
+transform = transforms.Compose([transforms.ToTensor()])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
