b70382156a68297721ef25fa82f262bdbbcea975
Octar123 <ranjanassmali123@gmail.com>
2025-07-31 21:20:06 +0530

    Added the git ignore file and got the output

--- main.py
+++ main.py
@@ -7,3 +7,18 @@ import numpy as np
 transform = transforms.Compose([transforms.ToTensor()])
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+def imgshow(img):
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+detailer = iter(trainloader)
+images, labels = next(detailer)
+
+imgshow(torchvision.utils.make_grid(images))
+print('Labels: ', ' '.join(f'{classes[labels[j]]:5s}' for j in range(4)))
